552bc7508523edd403244b1c53d82dcbeb61b373
Diwas Rimal <rimaldiwas10@gmail.com>
2024-01-22 22:49:38 +0545

    Modify run.sh

--- run.sh
+++ run.sh
@@ -5,9 +5,12 @@
 shared="src/messages/*.java src/utils/*.java"
 
 if [ "$1" = "server" ]; then
+    set -xe
     javac -d bin src/server/*.java $shared && 
         java -cp bin:lib/mysql-connector-j-8.3.0.jar server.Main 
+
 elif [ "$1" = "client" ]; then
+    set -xe
     javac -d bin src/client/*.java $shared &&
-        java -cp bin:lib/mysql-connector-j-8.3.0.jar client.Main
+        java -cp bin client.Main
 fi
